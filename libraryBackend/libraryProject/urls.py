98d0bea86064a562fe2631e3636cb142e2e527a3
Harshvartak <vartak.harsh@gmail.com>
2021-01-21 23:45:38 +0530

    Swagger docs updated

--- libraryBackend/libraryProject/urls.py
+++ libraryBackend/libraryProject/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
-        default_version="v1",
+        default_version="v3",
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
