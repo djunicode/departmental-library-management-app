2123f156695a9b4fbe752ce586c4d55afd99582d
aryan <aryanpathare281@gmail.com>
2021-02-05 19:27:42 +0530

    Created main activity, added navigation component, bottom navigation view, toolbar

---
+++ libraryAndroid/UnicodeLibraryApp/app/src/main/java/com/example/unicodelibraryapp/BookListFragment.java
@@ -0,0 +1,25 @@
+package com.example.unicodelibraryapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BookListFragment extends Fragment
+{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        Log.e("TST","BookList Frag");
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_book_list, container, false);
+    }
+}
